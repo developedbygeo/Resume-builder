f9754a6e62cfd1dcec7310eb5fdb9d658c182bc8
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-12 06:52:57 +0200

    Unused code cleanup:

--- src/components/shared/Input.styled.js
+++ src/components/shared/Input.styled.js
@@ -13,10 +13,6 @@ const inputAppear = keyframes`
 export const Input = styled.input`
   animation: ${inputAppear} 200ms ease-in-out;
   ${({ theme: { mixins } }) => mixins.inputStyle()}
-
-  &:focus {
-    border-color: #57b7df;
-  }
 `;
 
 export const ImgInput = styled(Input)`
@@ -30,6 +26,4 @@ export const ImgInput = styled(Input)`
 
 export const Label = styled.label`
   letter-spacing: 0.1rem;
-  font-weight: ${({ labelWeight }) => labelWeight || '300'};
-  color: ${({ labelColor }) => labelColor || '#475569'};
 `;
