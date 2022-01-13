2de6000a82059f4f3b80bbf0d55813a09553944f
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-13 11:07:02 +0200

    Updating the form to use the FormInput comp & the custom hook

--- src/components/Employment/EmploymentForm.js
+++ src/components/Employment/EmploymentForm.js
@@ -1,55 +1,136 @@
+import useInput from '../../hooks/useInput';
 import StyledForm from '../shared/Form.styled';
-import { Input, Label } from '../shared/Input.styled';
-import { Container } from '../shared/Container.styled';
+import FormInput from '../shared/FormInput';
+import FormButton from '../shared/FormButton';
 import Toggle from '../shared/Toggle/Toggle';
 
+const validateStartingDate = (val) => val;
+const validateEndingDate = (val) => val;
+const validateText = (val) => val.trim().length > 2;
+
 const EmploymentForm = () => {
+  const {
+    value: jobValue,
+    isValueValid: isJobValid,
+    hasError: jobHasError,
+    inputChangeHandler: jobChangeHandler,
+    inputBlurHandler: jobBlurHandler,
+  } = useInput(validateText);
+
+  const {
+    value: employerValue,
+    isValueValid: isEmployerValid,
+    hasError: employerHasError,
+    inputChangeHandler: employerChangeHandler,
+    inputBlurHandler: employerBlurHandler,
+  } = useInput(validateText);
+
+  const {
+    value: startingDateValue,
+    isValueValid: isStartingDateValid,
+    hasError: startingDateHasError,
+    inputChangeHandler: startingDateChangeHandler,
+    inputBlurHandler: startingDateBlurHandler,
+  } = useInput(validateStartingDate);
+
+  const {
+    value: endDateValue,
+    isValueValid: isEndDateValid,
+    hasError: endDateHasError,
+    inputChangeHandler: endDateChangeHandler,
+    inputBlurHandler: endDateBlurHandler,
+  } = useInput(validateEndingDate);
+
+  const {
+    value: jobLocationValue,
+    isValueValid: isJobLocationValid,
+    hasError: jobLocationHasError,
+    inputChangeHandler: jobLocationChangeHandler,
+    inputBlurHandler: jobLocationBlurHandler,
+  } = useInput(validateText);
+
+  const {
+    value: jobDescriptionValue,
+    isValueValid: isJobDescriptionValid,
+    hasError: jobDescriptionHasError,
+    inputChangeHandler: jobDescriptionChangeHandler,
+    inputBlurHandler: jobDescriptionBlurHandler,
+  } = useInput(validateText);
+
   return (
     <StyledForm>
-      <Container>
-        <Label htmlFor="title">Job Title</Label>
-        <Input
-          id="title"
-          type="text"
-          placeholder="eg. Software Developer"
-          required
-        />
-      </Container>
-      <Container>
-        <Label htmlFor="employer">Employer</Label>
-        <Input id="employer" type="text" placeholder="eg. Google" required />
-      </Container>
-      <Container>
-        <Label htmlFor="startdate">From</Label>
-        <Input id="startdate" type="date" required />
-      </Container>
-      <Container>
-        <Label htmlFor="enddate">To</Label>
-        <Input id="enddate" type="date" required />
-      </Container>
-      <Container>
-        <Label htmlFor="stillhere">Currently working here</Label>
-        <Toggle />
-      </Container>
-      <Container>
-        <Label htmlFor="joblocation">City</Label>
-        <Input
-          id="joblocation"
-          type="text"
-          required
-          placeholder="eg. Edinburgh"
-        />
-      </Container>
-      <Container>
-        <Label htmlFor="jobdescription">Description</Label>
-        <Input
-          id="jobdescription"
-          type="text"
-          as="textarea"
-          required
-          placeholder="Briefly provide your tasks and accomplishments"
-        />
-      </Container>
+      <FormInput
+        htmlFor="title"
+        inputId="title"
+        label="Job Title"
+        inputChange={jobChangeHandler}
+        inputBlur={jobBlurHandler}
+        inputValue={jobValue}
+        inputPlaceholder="eg. Software Engineer"
+        inputType="text"
+        required
+      />
+      <FormInput
+        htmlFor="employer"
+        inputId="employer"
+        label="Employer"
+        inputChange={employerChangeHandler}
+        inputBlur={employerBlurHandler}
+        inputValue={employerValue}
+        inputPlaceholder="eg. Google"
+        inputType="text"
+        required
+      />
+      <FormInput
+        htmlFor="startdate"
+        inputId="startdate"
+        label="From"
+        inputChange={startingDateChangeHandler}
+        inputBlur={startingDateBlurHandler}
+        inputValue={startingDateValue}
+        inputType="date"
+        required
+      />
+      <FormInput
+        htmlFor="enddate"
+        inputId="enddate"
+        label="To"
+        inputChange={endDateChangeHandler}
+        inputBlur={endDateBlurHandler}
+        inputValue={endDateValue}
+        inputType="date"
+        required
+      />
+      <FormInput
+        htmlFor="joblocation"
+        inputId="joblocation"
+        label="Location"
+        inputPlaceholder="eg. Edinburgh"
+        inputChange={jobLocationChangeHandler}
+        inputBlur={jobLocationBlurHandler}
+        inputValue={jobLocationValue}
+        inputType="text"
+        required
+      />
+      <FormInput
+        htmlFor="jobdescription"
+        inputId="jobdescription"
+        label="Description"
+        inputPlaceholder="Briefly describe your tasks and accomplishments"
+        inputChange={jobDescriptionChangeHandler}
+        inputBlur={jobDescriptionBlurHandler}
+        inputValue={jobDescriptionValue}
+        inputType="text"
+        inputAs="textarea"
+        required
+      />
+      <FormInput
+        isToggle={true}
+        htmlFor="stillHere"
+        inputId="stillHere"
+        label="Currently working here"
+      />
+      <FormButton />
     </StyledForm>
   );
 };
