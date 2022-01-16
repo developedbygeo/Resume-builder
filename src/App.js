4a01086bdddc1769e74a3a2e6874c612c047df67
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-16 22:23:09 +0200

    Wrapping core components with the InformationProvider

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ import { LayoutContainer } from './components/shared/Container.styled';
 import Contact from './components/Contact/Contact';
 import Employment from './components/Employment/Employment';
 import Education from './components/Education/Education';
+import InformationProvider from './store/infoContext';
 
 const App = () => {
   return (
     <ThemeProvider theme={themeSettings}>
-      <>
-        <GlobalStyle />
-        <Header />
-        <main>
-          <LayoutContainer>
-            <Personal />
-            <Contact />
-            <Employment />
-            <Education />
-          </LayoutContainer>
-        </main>
-      </>
+      <InformationProvider>
+        <>
+          <GlobalStyle />
+          <Header />
+          <main>
+            <LayoutContainer>
+              <Personal />
+              <Contact />
+              <Employment />
+              <Education />
+            </LayoutContainer>
+          </main>
+        </>
+      </InformationProvider>
     </ThemeProvider>
   );
 };
