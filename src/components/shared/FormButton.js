a491130179ed3725211810ead34431f3fcb0421e
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-17 02:48:25 +0200

    Adding additional props for button type

--- src/components/shared/FormButton.js
+++ src/components/shared/FormButton.js
@@ -1,14 +1,18 @@
 import { Container } from './Container.styled';
 import { CtaButton } from './Button.styled';
 
-const FormButton = ({ buttonText = 'Save', disabled = true }) => {
+const FormButton = ({
+  buttonText = 'Save',
+  disabled = true,
+  type = 'submit',
+}) => {
   const buttonMessage = disabled
     ? 'Please fill out the fields above to continue'
     : 'Save this form';
 
   return (
     <Container>
-      <CtaButton disabled={disabled} type="submit" title={buttonMessage}>
+      <CtaButton disabled={disabled} type={type} title={buttonMessage}>
         {buttonText}
       </CtaButton>
     </Container>
