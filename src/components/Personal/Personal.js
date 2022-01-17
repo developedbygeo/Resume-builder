2bbccb3f05f2d61c613f9a6719377c109fcb4d08
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-17 02:50:46 +0200

    Personal now will provide user feedback as soon as a form's been submitted

--- src/components/Personal/Personal.js
+++ src/components/Personal/Personal.js
@@ -3,26 +3,68 @@ import { StyledSection, Container } from '../shared/Container.styled';
 import { AddMoreDetails } from '../shared/Button.styled';
 import PersonalPhoto from './PersonalPhoto';
 import PersonalForm from './PersonalForm';
+import { CtaButton } from '../shared/Button.styled';
 import PersonalAdditionalForm from './PersonalAdditionalForm';
 
 const Personal = () => {
   const [toggleMoreDetails, setToggleMoreDetails] = useState(false);
+  const [persFormSubmitted, setPersFormSubmitted] = useState(false);
+  const [additionalFormSubmitted, setAdditionalFormSubmitted] = useState(false);
 
   const moreDetailsHandler = () => {
     setToggleMoreDetails((prevState) => !prevState);
   };
 
+  const checkSubmissionHandler = (status) => {
+    const formIdentifier =
+      'personalForm' in status
+        ? setPersFormSubmitted
+        : setAdditionalFormSubmitted;
+    formIdentifier(Object.values(status)[0]);
+  };
+
+  const retrievePFFormHandler = () => {
+    setPersFormSubmitted(false);
+  };
+
+  const retrieveAdditionalFormHandler = () => {
+    setAdditionalFormSubmitted(false);
+  };
+
+  const postSubmissionLayout = (field, retrieveFn) => {
+    return (
+      <Container className="editContainer" direction="row">
+        <h3>{field} submitted!</h3>
+        <CtaButton onClick={retrieveFn}>Edit</CtaButton>
+      </Container>
+    );
+  };
+
+  const personalFormLayout = !persFormSubmitted ? (
+    <Container direction="row">
+      <PersonalForm checkSubmission={checkSubmissionHandler} />
+      <PersonalPhoto />
+    </Container>
+  ) : (
+    postSubmissionLayout('Personal details', retrievePFFormHandler)
+  );
+
+  const additionalFormLayout = !additionalFormSubmitted ? (
+    <AddMoreDetails onClick={moreDetailsHandler}>
+      Toggle additional details ▼
+    </AddMoreDetails>
+  ) : (
+    postSubmissionLayout('Additional details', retrieveAdditionalFormHandler)
+  );
+
   return (
     <StyledSection marginTop="3rem">
       <h1>Personal Details</h1>
-      <Container direction="row">
-        <PersonalForm />
-        <PersonalPhoto />
-      </Container>
-      <AddMoreDetails onClick={moreDetailsHandler}>
-        Toggle additional details ▼
-      </AddMoreDetails>
-      {toggleMoreDetails && <PersonalAdditionalForm />}
+      {personalFormLayout}
+      {additionalFormLayout}
+      {toggleMoreDetails && (
+        <PersonalAdditionalForm checkSubmission={checkSubmissionHandler} />
+      )}
     </StyledSection>
   );
 };
