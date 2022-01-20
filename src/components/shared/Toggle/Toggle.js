3ae2c16496cff4aaf997178c751ffb9024943156
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-20 18:02:07 +0200

    Toggle now forwarding refs and passing the onChange prop to the input

--- src/components/shared/Toggle/Toggle.js
+++ src/components/shared/Toggle/Toggle.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import StyledToggle from './Toggle.styled';
 
-const Toggle = ({ title, id, onChange }) => {
+const Toggle = ({ title, id, onChange }, ref) => {
   return (
     <StyledToggle title={title || ''}>
-      <input type="checkbox" id={id} onChange={onChange} />
+      <input type="checkbox" id={id} onClick={onChange} ref={ref} />
       <span />
     </StyledToggle>
   );
 };
 
-export default Toggle;
+export default React.forwardRef(Toggle);
