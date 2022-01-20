87a8d448ef554dce9752c29657a3028e86f2a5d7
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-20 18:00:50 +0200

    Adding the useToggle custom hook

---
+++ src/hooks/useToggle.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+const useToggle = (currentEndDate) => {
+  const [endDateExists, setEndDateExists] = useState(false);
+  const [isToggleActivated, setIsToggleActivated] = useState(false);
+
+  useEffect(() => {
+    if (currentEndDate !== '' && !isToggleActivated) {
+      setEndDateExists(true);
+    } else if (currentEndDate === '' && isToggleActivated) {
+      setEndDateExists(true);
+    } else {
+      setEndDateExists(false);
+    }
+  }, [currentEndDate, isToggleActivated]);
+
+  const toggleChangeHandler = (e) => {
+    setIsToggleActivated(
+      e.target.checked === 'false' || e.target.checked === false ? false : true
+    );
+  };
+
+  return [endDateExists, toggleChangeHandler];
+};
+
+export default useToggle;
