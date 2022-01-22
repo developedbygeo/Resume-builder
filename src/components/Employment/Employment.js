5c3ccb938f9bf048efb435a4478141850b782ecf
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-22 15:05:57 +0200

    Noting possible solution

--- src/components/Employment/Employment.js
+++ src/components/Employment/Employment.js
@@ -2,11 +2,17 @@ import { Container, StyledSection } from '../shared/Container.styled';
 import EmploymentForm from './EmploymentForm';
 
 const Employment = () => {
+  // const additionalForm = (<EmploymentForm />)
+  // or map a state object and add forms based on that
+
+  console.log('re-evaluated');
+
   return (
     <StyledSection>
       <h1>Employment History</h1>
       <Container direction="row">
         <EmploymentForm />
+        {/* {additionalForm} */}
       </Container>
     </StyledSection>
   );
