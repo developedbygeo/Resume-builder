d9b7971ade4c1b7bdfe80c919ed44fab65dd9160
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-24 22:38:33 +0200

    Moving the new form logic in a new custom hook, which will be re-used in education as well

--- src/components/Employment/Employment.js
+++ src/components/Employment/Employment.js
@@ -1,60 +1,18 @@
-import _ from 'lodash';
-import { useState, useContext } from 'react';
-import { InfoContext } from '../../store/infoContext';
+import useExtra from '../../hooks/useExtra';
 import { Container, StyledSection } from '../shared/Container.styled';
-import { AddMoreDetails } from '../shared/Button.styled';
 import EmploymentForm from './EmploymentForm';
 
 const Employment = () => {
-  const {
-    defaultState: { information },
-  } = useContext(InfoContext);
-  const [originalFormSubmitted, setOriginalFormSubmitted] = useState(false);
-  const [showButton, setShowButton] = useState(false);
-  const [additionalForms, setAdditionalForms] = useState([]);
-
-  const submissionIdentifier = Object.keys(information).filter((key) =>
-    key.includes('employ')
-  ).length;
-
-  const submissionHandler = () => {
-    setOriginalFormSubmitted(true);
-    setShowButton(true);
-  };
-  const extraFormsSubmissionHandler = () => {
-    setShowButton(true);
-  };
-
-  const addNewHandler = () => {
-    setShowButton(false);
-    setAdditionalForms((prevState) => [
-      ...prevState,
-      <EmploymentForm
-        submissionCheck={extraFormsSubmissionHandler}
-        identifier={`emp-${submissionIdentifier + 1}`}
-      />,
-    ]);
-  };
-
-  const addNewFormBtn = originalFormSubmitted && showButton && (
-    <AddMoreDetails onClick={addNewHandler}>
-      Add more employment history
-    </AddMoreDetails>
-  );
-
-  const additionalForm = additionalForms.map((form, index) => (
-    <Container direction="row" key={index}>
-      {form}
-    </Container>
-  ));
+  const [originalSubmissionHandler, addNewFormBtn, formsToRender] =
+    useExtra('employment');
 
   return (
     <StyledSection>
       <h1>Employment History</h1>
       <Container direction="row">
-        <EmploymentForm submissionCheck={submissionHandler} />
+        <EmploymentForm submissionCheck={originalSubmissionHandler} />
       </Container>
-      {additionalForm}
+      {formsToRender}
       {addNewFormBtn}
     </StyledSection>
   );
---
+++ src/hooks/useExtra.js
@@ -0,0 +1,66 @@
+import { useState, useContext } from 'react';
+import { InfoContext } from '../store/infoContext';
+import EmploymentForm from '../components/Employment/EmploymentForm';
+import EducationForm from '../components/Education/EducationForm';
+import { Container } from '../components/shared/Container.styled';
+import { AddMoreDetails } from '../components/shared/Button.styled';
+
+const useExtra = (componentIdentifier) => {
+  const [originalFormSubmitted, setOriginalFormSubmitted] = useState(false);
+  const [showButton, setShowButton] = useState(false);
+  const {
+    defaultState: { information },
+  } = useContext(InfoContext);
+  const [additionalForms, setAdditionalForms] = useState([]);
+
+  const originalSubmissionHandler = () => {
+    setOriginalFormSubmitted(true);
+    setShowButton(true);
+  };
+
+  const extraSubmissionHandler = () => {
+    setShowButton(true);
+  };
+
+  const ctxIdentifier = componentIdentifier === 'employment' ? 'employ' : 'edu';
+
+  const buttonText = ctxIdentifier === 'employ' ? 'employment' : 'education';
+
+  const newFormIdentifier = Object.keys(information).filter((key) =>
+    key.includes(ctxIdentifier)
+  ).length;
+
+  const componentToRender =
+    ctxIdentifier === 'employ' ? (
+      <EmploymentForm
+        submissionCheck={extraSubmissionHandler}
+        identifier={`emp-${newFormIdentifier + 1}`}
+      />
+    ) : (
+      <EducationForm
+        submissionCheck={extraSubmissionHandler}
+        identifier={`edu-${newFormIdentifier + 1}`}
+      />
+    );
+
+  const addNewEntryHandler = () => {
+    setShowButton(false);
+    setAdditionalForms((prevState) => [...prevState, componentToRender]);
+  };
+
+  const addNewFormBtn = originalFormSubmitted && showButton && (
+    <AddMoreDetails onClick={addNewEntryHandler}>
+      Add more {buttonText} history
+    </AddMoreDetails>
+  );
+
+  const formsToRender = additionalForms.map((form, index) => (
+    <Container direction="row" key={index}>
+      {form}
+    </Container>
+  ));
+
+  return [originalSubmissionHandler, addNewFormBtn, formsToRender];
+};
+
+export default useExtra;
