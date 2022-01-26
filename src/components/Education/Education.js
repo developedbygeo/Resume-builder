0c5b4a2ad36ee5e0d4fe9aa11c76c60264a29fcc
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-26 03:04:21 +0200

    Wiring up the Education component to use the useExtra hook for extra form rendering

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,13 +1,19 @@
+import useExtra from '../../hooks/useExtra';
 import { Container, StyledSection } from '../shared/Container.styled';
 import EducationForm from './EducationForm';
 
 const Education = () => {
+  const [originalSubmissionHandler, addNewFormBtn, formsToRender] =
+    useExtra('education');
+
   return (
     <StyledSection>
       <h1>Education</h1>
       <Container direction="row">
-        <EducationForm />
+        <EducationForm submissionCheck={originalSubmissionHandler} />
       </Container>
+      {formsToRender}
+      {addNewFormBtn}
     </StyledSection>
   );
 };
