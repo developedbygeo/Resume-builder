691d895f273b5ebfcda42ea6625a5bb2591d15c4
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-28 02:07:11 +0200

    Once the form has been submitted, the toggle button no longer appears

--- src/components/ExtraInformation/ExtraInformation.js
+++ src/components/ExtraInformation/ExtraInformation.js
@@ -7,28 +7,38 @@ import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 const ExtraInformation = () => {
   const [toggleExtra, setToggleExtra] = useState(false);
+  const [shouldToggleShow, setShouldToggleShow] = useState(true);
+
+  const submissionCheckHandler = () => {
+    setShouldToggleShow(false);
+  };
 
   const toggleExtraHandler = () => {
     setToggleExtra((prevState) => !prevState);
   };
+
   const toggleIcon = toggleExtra ? (
     <FontAwesomeIcon icon={faChevronUp} />
   ) : (
     <FontAwesomeIcon icon={faChevronDown} />
   );
 
+  const buttonToggle = shouldToggleShow && (
+    <AddMoreDetails onClick={toggleExtraHandler}>
+      Toggle skills & languages (optional) {toggleIcon}
+    </AddMoreDetails>
+  );
+
   const showFormLayout = toggleExtra && (
     <Container direction="row">
-      <ExtraInformationForm />
+      <ExtraInformationForm checkSubmission={submissionCheckHandler} />
     </Container>
   );
 
   return (
     <StyledSection>
       <h1>Skills & Languages</h1>
-      <AddMoreDetails onClick={toggleExtraHandler}>
-        Toggle skills & languages (optional) {toggleIcon}
-      </AddMoreDetails>
+      {buttonToggle}
       {showFormLayout}
     </StyledSection>
   );
