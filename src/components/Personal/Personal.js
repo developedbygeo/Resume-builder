df3e0cfa76932c6f1ed3e5b80a3200dc0167d2fd
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-29 21:45:05 +0200

    Passing marginTop prop to Container

--- src/components/Personal/Personal.js
+++ src/components/Personal/Personal.js
@@ -36,7 +36,7 @@ const Personal = () => {
   return (
     <StyledSection marginTop="3rem">
       <h1>Personal Details</h1>
-      <Container direction="row">
+      <Container direction="row" marginTop={'2rem'}>
         <PersonalForm />
         <PersonalPhoto />
       </Container>
