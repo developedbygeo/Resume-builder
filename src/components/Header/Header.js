0520ce723326de76f54582bfad648f7858eec4d4
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-30 15:55:13 +0200

    Adding the logic behind toggling the modal for both purposes - to clear the forms or to preview the cv

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,36 @@
+import { useState } from 'react';
 import { DownloadButton, TrashButton } from './NavButtons';
 import { Nav, StyledHeader } from './Header.styled';
 import { Logo } from './Logo.styled';
 import { LogoContainer } from '../shared/Container.styled';
 import logoImg from '../../assets/logo.svg';
+import Modal from '../shared/Modal/Modal';
+import { ClearFormsDialogue, PreviewDialogue } from '../shared/Modal/Dialogues';
 
 const Header = () => {
+  const [modalState, setModalState] = useState(false);
+  const [previewState, setPreviewState] = useState(false);
+
+  const modalToggleHandler = () => {
+    setModalState((prevState) => !prevState);
+  };
+
+  const previewToggleHandler = () => {
+    setPreviewState((prevState) => !prevState);
+  };
+
+  const deleteModalView = modalState && (
+    <Modal onDisable={modalToggleHandler} deleteMenu="true">
+      <ClearFormsDialogue onDisable={modalToggleHandler} />
+    </Modal>
+  );
+
+  const previewModalView = previewState && (
+    <Modal previewMenu="true" onDisable={previewToggleHandler}>
+      <PreviewDialogue />
+    </Modal>
+  );
+
   return (
     <StyledHeader>
       <div>
@@ -13,11 +39,13 @@ const Header = () => {
         </LogoContainer>
         <Nav>
           <div>
-            <TrashButton />
-            <DownloadButton />
+            <TrashButton onModalToggle={modalToggleHandler} />
+            <DownloadButton onPreviewToggle={previewToggleHandler} />
           </div>
         </Nav>
       </div>
+      {deleteModalView}
+      {previewModalView}
     </StyledHeader>
   );
 };
