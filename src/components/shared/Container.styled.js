5c31612f7af20e465a626c74fd27f494cb6bba38
georgiosan <georgiosanagnostou@hotmail.com>
2022-01-31 23:35:22 +0200

    Adding conditional layout for the container and styling the aside and its children

--- src/components/shared/Container.styled.js
+++ src/components/shared/Container.styled.js
@@ -1,15 +1,33 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import placeholder from '../../assets/placeholder.png';
 
-export const Container = styled.div`
-  width: 95%;
-  height: 95%;
-  margin: auto;
+const flexLayout = css`
   display: flex;
   flex-direction: ${({ direction }) => direction || 'column'};
   justify-content: center;
   margin-top: ${({ marginTop }) => marginTop || '1rem'};
   align-items: ${({ alignItems }) => alignItems || 'center'};
+`;
+
+const customGridLayout = css`
+  display: grid;
+  grid-template-rows: ${({ gridRows }) => gridRows || 'auto'};
+  grid-template-columns: ${({ gridColumns }) => gridColumns || '1fr'};
+`;
+
+const getLayout = (props) => {
+  if (props.grid) {
+    return customGridLayout;
+  } else {
+    return flexLayout;
+  }
+};
+
+export const Container = styled.div`
+  width: ${({ contWidth }) => contWidth || '95%'};
+  height: 95%;
+  margin: auto;
+  ${getLayout}
 `;
 
 export const LogoContainer = styled.div`
@@ -48,7 +66,7 @@ export const StyledSection = styled.section`
     padding: 1rem;
   }
 
-  .photoCont {
+  & .photoCont {
     position: absolute;
     top: 13%;
     right: 5%;
@@ -73,21 +91,29 @@ export const PreviewContainer = styled(Container)`
   width: 100%;
   margin: 0;
   ${({ theme: { mixins } }) =>
-    mixins.gridMixin('0.4fr 1fr', '1fr', 'center', 'space-evenly')};
+    mixins.gridMixin('0.58fr 1fr', '1fr', 'center', 'space-evenly')};
 
   & > aside {
     grid-area: 1;
   }
 `;
 
+const asideText = css`
+  width: 100%;
+  font-size: 1.6rem;
+  color: ${({ theme }) => theme.colors.main};
+  overflow-wrap: anywhere;
+`;
+
 export const StyledAside = styled.aside`
   width: 100%;
   height: 100%;
+  max-height: 80vh;
   margin-top: auto;
   ${({ theme: { mixins } }) =>
     mixins.gridMixin('1fr', '0.7fr repeat(3, 1fr)', 'center', 'space-evenly')};
   background-color: ${({ theme }) => theme.colors.cta.standard};
-  border-radius: 1rem;
+  border-radius: 0;
 
   & > div {
     word-wrap: anywhere;
@@ -100,18 +126,44 @@ export const StyledAside = styled.aside`
     }
   }
   & > .img-container {
-    height: 80%;
-    width: 90%;
+    height: 90%;
+    width: 80%;
     border-radius: 50%;
     border: 2px solid red;
     margin: auto;
   }
   .headerWrapper {
-    width: 100%;
+    width: 95%;
     height: 3rem;
     background: ${({ theme }) => theme.colors.main};
     color: ${({ theme }) => theme.colors.headerTitles};
-    border-radius: 1rem;
-    padding: 0.25rem;
+    border-radius: 0 0.5rem 0.5rem 0;
+    padding: 0.25rem 0.5rem;
+  }
+  .wrapCont {
+    place-items: center;
+    gap: 0.5rem;
+    svg {
+      font-size: 1.6rem;
+      color: ${({ theme }) => theme.colors.headerTitles};
+    }
+    p {
+      ${asideText}
+    }
+  }
+  .skills {
+    width: 80%;
+    ${({ theme: { mixins } }) =>
+      mixins.gridMixin('1fr', 'auto', 'center', 'center')};
+    justify-self: center;
+    align-self: flex-start;
+    gap: 1rem;
+    & > li {
+      text-transform: capitalize;
+      ${asideText}
+      &::marker {
+        color: ${({ theme }) => theme.colors.headerTitles};
+      }
+    }
   }
 `;
