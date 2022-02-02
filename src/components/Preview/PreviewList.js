d227aeee79f6c67e037a088575378e76721c768e
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-02 20:09:55 +0200

    Merging into one, modular component

---
+++ src/components/Preview/PreviewList.js
@@ -0,0 +1,18 @@
+import { Container } from '../shared/Container.styled';
+
+const PreviewList = ({ items, title }) => {
+  const renderedItems = items.map((item, index) => {
+    return <li key={index}>{item}</li>;
+  });
+
+  return (
+    <Container grid={true} contWidth="100%">
+      <div className="headerWrapper">
+        <h4>{title}</h4>
+      </div>
+      <ul className="skills">{renderedItems}</ul>
+    </Container>
+  );
+};
+
+export default PreviewList;
